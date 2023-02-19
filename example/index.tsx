7b191d89d9cb60ad0ba28bf264e3b1554e6b3ebf
Sejmou <samo.kolter@gmail.com>
2023-02-19 22:19:07 +0100

    update package.json of example library to also include minified Tailwind CSS in built package; switch to yarn for usage example

--- example/index.tsx
+++ example/index.tsx
@@ -1,6 +1,7 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import 'example-component-lib/dist/tailwind.css';
 import { Thing } from '../.';
 
 const App = () => {
